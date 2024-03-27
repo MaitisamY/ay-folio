e8161971ba372ec4ae0b9b91a6c79abb9c1cbea3
Muhammad Aitisam Yaseen <m.aitisamyaseen@gmail.com>
2024-03-27 13:31:10 +0500

    add main.css and Main.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,21 @@
+/* eslint-disable react/no-unknown-property */
 import { lazy, Suspense } from 'react'
 const Header = lazy(() => import('../partials/Header'))
+const Main = lazy(() => import('../partials/Main'))
 
 export default function Home() {
     return (
         <Suspense 
             fallback={
-                <div class="loading-screen">
-                    <div class="loader"></div>
+                <div className="loading-screen">
+                    <div className="loader"></div>
                 </div>
             }
         >
         <Header />
+        <Main>
+            
+        </Main>
         </Suspense>
     )
 }
